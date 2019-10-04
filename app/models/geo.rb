b51c0e1ddb92f2af699419ec1b8e26631a87b3d9
Krzysztof Peksa <krzysztof@peksa.pro>
2019-10-04 13:28:52 +0200

    added test form geo model

--- app/models/geo.rb
+++ app/models/geo.rb
@@ -1,7 +1,9 @@
+require 'resolv'
+
 class Geo < ApplicationRecord
   validates :ip, uniqueness: true
   validates :ip, :format => {
       :with => Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex)
   }
-  validates_format_of :city, :country, :with => /^[a-z]+$/i
+  validates_format_of :city, :country, :with => /\A[a-z]+\z/i
 end
--- test/models/geo_test.rb
+++ test/models/geo_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 
 class GeoTest < ActiveSupport::TestCase
   test 'geo with valid ip should be valid' do
-    geo = Geo.new(ip: '333.333.333.333',city:'Gdansk',country:'Poland')
-    assert geo.valid?
+    geo = Geo.new(ip: '192.168.333.444',city:'Gdansk',country:'Poland')
+    assert_not geo.valid?
+  end
+  test 'geo with not correct city should not be valid' do
+    geo = Geo.new(ip: '192.168.0.1',city:'Gdansk123',country:'Poland')
+    assert_not geo.valid?
+  end
+  test 'geo with not correct country should not be valid' do
+    geo = Geo.new(ip: '192.168.0.1',city:'Gdansk',country:'Poland123')
+    assert_not geo.valid?
   end
 end
